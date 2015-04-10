b311b40e9c0f13659d5300f71d7c9e2b371bb4ce
Neil Oler <neiloler@gmail.com>
2015-04-10 11:01:55 -0600

    Playing around with longestTitle and shortestTitle data sets.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,12 +1,13 @@
 class SoundsController < ApplicationController
 
   def index
-    hottestSongs = Sound.order(download_count: :desc, favoritings_count: :desc, comment_count: :desc, playback_count: :desc)
+    popularSortedSongs = Sound.order(download_count: :desc, favoritings_count: :desc, comment_count: :desc, playback_count: :desc)
 
     # How does length of Title relate to playback count?
     # sortedSounds = Sound.where("title != ''").sort({ |x,y| y.title.length <=> x.title.length }).order(:playback_count)
 
-    @longestTitleSounds = hottestSongs.first(10).sort_by { |v| v.title.length }.reverse
+    @longestTitleSounds = popularSortedSongs.first(500).sort_by { |v| v.title.length }.reverse[0..9]
+    @shortestTitleSounds = popularSortedSongs.first(500).sort_by { |v| v.title.length }[0..9]
 
     # Does ellipsing off the end of the title hurt a song?
 
