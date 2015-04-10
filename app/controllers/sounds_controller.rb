31f6621a52592e6664956624012319828b3fee4b
Neil Oler <neiloler@gmail.com>
2015-04-09 23:48:05 -0600

    Messing with data slicing.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,13 +1,12 @@
 class SoundsController < ApplicationController
 
   def index
-    sort_order = "download_count, favoritings_count, comment_count, playback_count"
-
+    hottestSongs = Sound.order(download_count: :desc, favoritings_count: :desc, comment_count: :desc, playback_count: :desc)
 
     # How does length of Title relate to playback count?
-    sortedSounds = Sound.where("title != ''").sort { |x,y| y.title.length <=> x.title.length }
-    @longestTitleSounds = sortedSounds.first(100)
-    @shortestTitleSounds = sortedSounds.last(100)
+    # sortedSounds = Sound.where("title != ''").sort({ |x,y| y.title.length <=> x.title.length }).order(:playback_count)
+
+    @longestTitleSounds = hottestSongs.first(100).sort_by { |v| v.title.length }.reverse
 
     # Does ellipsing off the end of the title hurt a song?
 
