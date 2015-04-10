ba3a3a284e746ba7f4c30c75b093922a4d24d8bf
Neil Oler <neiloler@gmail.com>
2015-04-10 01:00:33 -0600

    Decrease number of longestTitleSongs (for now).

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -6,16 +6,11 @@ class SoundsController < ApplicationController
     # How does length of Title relate to playback count?
     # sortedSounds = Sound.where("title != ''").sort({ |x,y| y.title.length <=> x.title.length }).order(:playback_count)
 
-    @longestTitleSounds = hottestSongs.first(100).sort_by { |v| v.title.length }.reverse
+    @longestTitleSounds = hottestSongs.first(10).sort_by { |v| v.title.length }.reverse
 
     # Does ellipsing off the end of the title hurt a song?
 
-    # If genre is contained in tag list, does this result in higher favorites
-    #   Get songs that have genre/tag matches, sort by sort_order
-
-
-    #   Get top ten songs (maybe get bottom 10 as well?)
-    # stuff.first(10)
+    # If genre is contained in tag list, does this result in higher favorites?
 
     # If a song is allowed to be downloaded, does that help? Which genre?
     # 
