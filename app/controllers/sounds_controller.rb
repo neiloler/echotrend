f9368e927cb4499cb886d7055a46993980367f3c
Neil Oler <neiloler@gmail.com>
2015-04-10 11:29:38 -0600

    Remove sorting on top tracks, as we want most popular, must let user decide on what data means to them.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -6,8 +6,7 @@ class SoundsController < ApplicationController
     # How does length of Title relate to playback count?
     # sortedSounds = Sound.where("title != ''").sort({ |x,y| y.title.length <=> x.title.length }).order(:playback_count)
 
-    @longestTitleSounds = popularSortedSongs.first(500).sort_by { |v| v.title.length }.reverse[0..9]
-    @shortestTitleSounds = popularSortedSongs.first(500).sort_by { |v| v.title.length }[0..9]
+    @mostPopularSongs = popularSortedSongs.first(25)
 
     # Does ellipsing off the end of the title hurt a song?
 
