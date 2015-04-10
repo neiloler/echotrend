4cfe6338f80d81b8587d6c5c84a871c7bb650d41
Neil Oler <neiloler@gmail.com>
2015-04-10 14:28:11 -0600

    Making sure that we're defining 'popular' as something has been downloaded, listened to, etc.

--- app/controllers/sounds_controller.rb
+++ app/controllers/sounds_controller.rb
@@ -1,7 +1,7 @@
 class SoundsController < ApplicationController
 
   def index
-    popularSortedSongs = Sound.order(download_count: :desc, favoritings_count: :desc, comment_count: :desc, playback_count: :desc)
+    popularSortedSongs = Sound.where("download_count > 0 AND favoritings_count > 0 AND comment_count > 0 AND playback_count > 0").order(download_count: :desc, favoritings_count: :desc, comment_count: :desc, playback_count: :desc)
 
     # How does length of Title relate to playback count?
     # sortedSounds = Sound.where("title != ''").sort({ |x,y| y.title.length <=> x.title.length }).order(:playback_count)
